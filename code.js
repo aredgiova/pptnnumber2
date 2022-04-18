a92f122e2d9a3eb72de7b957bf63edea827b4c44
aredgiova <ared.lopez@gmail.com>
2022-04-18 00:03:20 -0500

    Add files via upload

---
+++ code.js
@@ -0,0 +1,46 @@
+console.log("Mitesoro");
+
+
+var GBP_USD = 21.32565;
+var EUR_USD = 21.32565;
+var USD_YEN = 21.32565;
+var GBP_EUR = 21.32565;
+var EUR_YEN = 21.32565;
+var GBP_YEN = 21.32565;
+
+var GBP_USD_P= 22.32565;
+var EUR_USD_P= 22.32565;
+var USD_YEN_P= 22.32565;
+var GBP_EUR_P= 22.32565;
+var EUR_YEN_P= 22.32565;
+var GBP_YEN_P= 22.32565;
+
+var GBP_USD_N= 20.32565;
+var EUR_USD_N= 20.32565;
+var USD_YEN_N= 20.32565;
+var GBP_EUR_N= 20.32565;
+var EUR_YEN_N= 20.32565;
+var GBP_YEN_N= 20.32565;
+
+
+
+document.getElementById("gbp_usd").innerHTML = GBP_USD;
+document.getElementById("eur_usd").innerHTML = EUR_USD;
+document.getElementById("usd_yen").innerHTML = USD_YEN;
+document.getElementById("gbp_eur").innerHTML = GBP_EUR;
+document.getElementById("eur_yen").innerHTML = EUR_YEN;
+document.getElementById("gbp_yen").innerHTML = GBP_YEN;
+
+document.getElementById("gbp_usd_p").innerHTML = GBP_USD_P;
+document.getElementById("eur_usd_p").innerHTML = EUR_USD_P;
+document.getElementById("usd_yen_p").innerHTML = USD_YEN_P;
+document.getElementById("gbp_eur_p").innerHTML = GBP_EUR_P;
+document.getElementById("eur_yen_p").innerHTML = EUR_YEN_P;
+document.getElementById("gbp_yen_p").innerHTML = GBP_YEN_P;
+
+document.getElementById("gbp_usd_n").innerHTML = GBP_USD_N;
+document.getElementById("eur_usd_n").innerHTML = EUR_USD_N;
+document.getElementById("usd_yen_n").innerHTML = USD_YEN_N;
+document.getElementById("gbp_eur_n").innerHTML = GBP_EUR_N;
+document.getElementById("eur_yen_n").innerHTML = EUR_YEN_N;
+document.getElementById("gbp_yen_n").innerHTML = GBP_YEN_N;
